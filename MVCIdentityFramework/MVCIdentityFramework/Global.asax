c595dd52c46f456b0be794f4f210658d1d9bdfe1
Snehasish Pramanik <snehasish.pramanik@nagarro.com>
2021-09-01 11:24:29 +0530

    Practice identity framework

---
+++ MVCIdentityFramework/MVCIdentityFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCIdentityFramework.MvcApplication" Language="C#" %>
---
+++ MVCIdentityFramework/MVCIdentityFramework/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCIdentityFramework.Startup))]
+namespace MVCIdentityFramework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
