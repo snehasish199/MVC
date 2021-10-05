b7d836386f953f1e132ee8101082cf04419d588f
Snehasish Pramanik <snehasish.pramanik@nagarro.com>
2021-10-05 11:05:37 +0530

    Singleton design pattern demo

---
+++ SingletonDemo/SignletoneDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SignletoneDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton SingletonObj = Singleton.GetInstance();
+            SingletonObj.PrintDetails("Hello from 1st invocation");
+
+            Singleton Obj = Singleton.GetInstance();
+            Obj.PrintDetails("Hello from 2nd invocation");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonDemo/SignletoneDemo/Singleton.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignletoneDemo
+{
+   public sealed class Singleton
+    {
+        private static int count = 0;
+        private Singleton()
+        {
+            //to check how many times constructor is called.
+            count++;
+            Console.WriteLine(count+" time constructor call is ");
+
+        }
+        //lazy initialization
+        // type 1: using lazy keyword
+        private static readonly Lazy<Singleton> instance =
+            new Lazy<Singleton>(()=>new  Singleton());
+        public static Singleton GetInstance()
+        {
+            return instance.Value;
+        }
+
+        //type 2: using Lock 
+
+        //private static readonly Singleton instance=null;
+        //private static readonly object Obj = new object();
+        //public static Singleton GetInstance()
+        //{
+        //    if (instance == null)
+        //    {
+        //        lock (obj)
+        //        {
+        //            if (instance == null)
+        //            {
+        //                instance = new Singleton();
+        //            }
+
+        //        }
+
+        //    }
+        //    return instance;    
+        //}
+
+
+
+        //Eager Initialization
+
+        //private static Singleton instance = new Singleton();
+        //public static Singleton GetInstance()
+        //{
+        //    return instance;
+        //}
+
+
+          public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
