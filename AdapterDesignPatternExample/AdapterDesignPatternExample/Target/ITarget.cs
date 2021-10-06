10a9fa70c12c351eb180b30468761caf02ea7d3f
Snehasish Pramanik <snehasish.pramanik@nagarro.com>
2021-10-06 10:14:10 +0530

    Perform adapter design pattern

---
+++ AdapterDesignPatternExample/AdapterDesignPatternExample/Target/ITarget.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdapterDesignPatternExample.Target
+{
+    public interface ITarget
+    {
+       
+            void ProcessCompanySalary(string[,] employeesArray);
+
+    }
+}
